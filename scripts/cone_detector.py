236c9aefac51efe1236783add514def3c3ceb5cb
Eric Gonzalez <ericgon456@Erics-MBP.lan>
2022-03-07 17:41:52 -0500

    refactored code

--- scripts/cone_detector.py
+++ scripts/cone_detector.py
@@ -11,7 +11,7 @@ from geometry_msgs.msg import Point  # geometry_msgs not in CMake file
 from visual_servoing.msg import ConeLocationPixel
 
 # import your color segmentation algorithm; call this function in ros_image_callback!
-# from computer_vision.color_segmentation import cd_color_segmentation
+from computer_vision.color_segmentation import cd_color_segmentation
 
 
 class ConeDetector():
